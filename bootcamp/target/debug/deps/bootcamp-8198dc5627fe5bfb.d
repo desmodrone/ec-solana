52d34ca961d680e9954789762884d4744fb6ee09
desmo <62178867+desmodrone@users.noreply.github.com>
2023-09-05 15:29:45 +0200

    Untrack SolanaBootcamp directory

---
+++ bootcamp/target/debug/deps/bootcamp-8198dc5627fe5bfb.d
@@ -0,0 +1,5 @@
+/Users/desmo/repos/ec-solana/bootcamp/target/debug/deps/bootcamp-8198dc5627fe5bfb.rmeta: src/main.rs
+
+/Users/desmo/repos/ec-solana/bootcamp/target/debug/deps/bootcamp-8198dc5627fe5bfb.d: src/main.rs
+
+src/main.rs:
---
+++ bootcamp/target/debug/deps/bootcamp-f0f3a06ca23d9099.d
@@ -0,0 +1,5 @@
+/Users/desmo/repos/ec-solana/bootcamp/target/debug/deps/bootcamp-f0f3a06ca23d9099.rmeta: src/main.rs
+
+/Users/desmo/repos/ec-solana/bootcamp/target/debug/deps/bootcamp-f0f3a06ca23d9099.d: src/main.rs
+
+src/main.rs:
